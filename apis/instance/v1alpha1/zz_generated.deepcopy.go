273284813ea83dba1b33d26bf142533aa377b0ef
lgtm <1gtm@users.noreply.github.com>
2021-10-29 04:21:04 -0700

    Generate code for provider@v1.19.1 gen@9311716 (#15)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- apis/instance/v1alpha1/zz_generated.deepcopy.go
+++ apis/instance/v1alpha1/zz_generated.deepcopy.go
@@ -1095,11 +1095,6 @@ func (in *IpSpecResource) DeepCopyInto(out *IpSpecResource) {
 		*out = new(string)
 		**out = **in
 	}
-	if in.ApplyImmediately != nil {
-		in, out := &in.ApplyImmediately, &out.ApplyImmediately
-		*out = new(bool)
-		**out = **in
-	}
 	if in.Gateway != nil {
 		in, out := &in.Gateway, &out.Gateway
 		*out = new(string)
